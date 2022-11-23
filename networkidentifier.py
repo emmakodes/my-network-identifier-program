d83333fadfa673a1de708676ef1489e09ff7bfc9
emmakodes <emmamichael65@gmail.com>
2022-11-23 11:40:57 +0100

    Add new numbers

--- networkidentifier.py
+++ networkidentifier.py
@@ -1,7 +1,7 @@
 list_of_mtn_numbers = ["0803", "0806", "0810", "0813", "0814", "0816", "0903",
        "0906", "0913", "0916", "0702", "0703", "0704", "0706"]
 
-list_of_glo_numbers = ["0915", "0905", "0815", "0811", "0807", "0805"]
+list_of_glo_numbers = ["0915", "0905", "0815", "0811", "0807", "0805", "84848"]
 
 list_of_airtel_numbers = ["0701", "0708", "0802", "0808", "0812",
                           "0901", "0902", "0904", "0907", "0912"]
@@ -19,5 +19,7 @@ elif users_input in list_of_etisalat_numbers:
     print("Your number is ETISALAT")
 else:
     print("Sorry, Invalid number")
+
+print("I am still coding")
 
 
